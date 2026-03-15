f373974e2374252b534645ee8cb25f1f454a60d0
Madhu Cherukuri <cherukurimadhu52@gmail.com>
2026-03-15 19:38:04 +0530

    verticalOrder

---
+++ day-25.py
@@ -0,0 +1,31 @@
+
+
+class Solution:
+    def verticalOrder(self, root): 
+        if not root:
+            return []
+        
+        mp = defaultdict(list)
+        q = deque([(root, 0)])
+        
+        min_hd = 0
+        max_hd = 0
+        
+        while q:
+            node, hd = q.popleft()
+            
+            mp[hd].append(node.data)
+            
+            min_hd = min(min_hd, hd)
+            max_hd = max(max_hd, hd)
+            
+            if node.left:
+                q.append((node.left, hd - 1))
+            if node.right:
+                q.append((node.right, hd + 1))
+        
+        res = []
+        for i in range(min_hd, max_hd + 1):
+            res.append(mp[i])
+            
+        return res
